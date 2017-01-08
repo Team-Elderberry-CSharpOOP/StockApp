5e5a606c6fa407fd21f18fd4e8a7aa9ab11caffa
nicke <ndvalkov@abv.bg>
2017-01-08 09:46:49 +0200

    Added starter project (Stock info provider, Authentication, Login page, other)

---
+++ StockApp/Utils/StockInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace StockApp.Utils
+{
+    public class StockInfo
+    {
+        public string Price { get; set; }
+        public string Name { get; set; }
+        public string Currency { get; set; }
+    }
+}
